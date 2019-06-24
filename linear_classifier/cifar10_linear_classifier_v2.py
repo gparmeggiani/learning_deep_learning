Fix a bug in the gradient calculation
A factor 1/n was missing in the computation of the dW
Code cleanup

linear_classifier/cifar10_linear_classifier_v2.py
@@ -5,26 +5,44 @@ Giacomo Parmeggiani <giacomo.parmeggiani@gmail.com>
 June 2019
 
 Train a simple linear classifier on the CIFAR-10 dataset.
+Based on the CS231n class material
 
 """
+import sys
 import pickle
 import numpy as np 
 import matplotlib.pyplot as plt
 
 
-def unpickle(file):
-    with open(file, 'rb') as fo:
-        dict = pickle.load(fo, encoding='bytes')
-    return dict
+def unpickle(file_name):
+    """
+    Unpickle the data stored in a file and return the corresponding dictionary
+
+    :param file_name:
+    :return:
+    """
+    with open(file_name, 'rb') as fo:
+        return pickle.load(fo, encoding='bytes')
 
 
 def plot_img(img):
-        plt.figure(figsize=(4, 4))
-        plt.imshow(img[:3072].reshape((3,32,32)).transpose((1, 2, 0)).astype('uint8'))
-        plt.show()
+    """
+    Plot an image represented as a 1D vector of 3072 elements by reconstructing the original 32x32x3 image
+    :param img:
+    :return:
+    """
+    plt.figure(figsize=(4, 4))
+    plt.imshow(img[:3072].reshape((3, 32, 32)).transpose((1, 2, 0)).astype('uint8'))
+    plt.show()
 
 
 def plot_W(W):
+    """
+    Plot the weights in a single matplotlib figure
+
+    :param W: The weights matrix
+    :return:
+    """
 
     w = W[:, :-1]
     
@@ -36,7 +54,7 @@ def plot_W(W):
         # Rescale the weights to be between 0 and 255
         wimg = 255.0 * (w[i] - w_min) / (w_max - w_min)
 
-        plt.imshow(wimg.reshape((3,32,32)).transpose((1, 2, 0)).astype('uint8'))
+        plt.imshow(wimg.reshape((3, 32, 32)).transpose((1, 2, 0)).astype('uint8'))
         plt.axis('off')
         plt.title(classes[i])
 
@@ -44,15 +62,22 @@ def plot_W(W):
 
 
 def main():
+    """
+    This loads the dataset and does the data preprocessing
+    Then it instantiates the linear classifier class and starts the training loop
 
+    :return:
+    """
     minibatch_size = 256
-    stats_rate = 1
+    stats_rate = 10
+    num_iterations = 2010
 
     plt.ion()
 
     dataset = []
     labels = []
 
+    # Loading datasets
     for i in range(4):
         filename = "../datasets/cifar-10-batches-py/data_batch_{}".format(i+1)
         print("Loading {}".format(filename))
@@ -65,21 +90,26 @@ def main():
     dataset = np.concatenate(dataset)
     labels = np.concatenate(labels)
 
+    # Normalize data: subtract the mean image
     mean_image = np.mean(dataset, axis=0)
     dataset -= mean_image
 
+    # Augment the dataset with a column on ones, in order to include the bias term in the W matrix
     biases = np.ones((dataset.shape[0], 1))
     dataset = np.hstack((dataset, biases))
 
+    # Instantiate the linear classifier class
     lc = LinearClassifier()
 
+    # The training loop
     loss_history = []
     accuracy_history = []
     avg_loss = 0
     avg_accuracy = 0
-    epoch = 0
-    while True: 
+    iteration = 0
+    while iteration < num_iterations:
 
+        # Extract the minibatch and train the classifier
         minibatch_idx = np.random.randint(len(labels)-minibatch_size)
 
         loss, accuracy = lc.train(
@@ -87,16 +117,17 @@ def main():
             y=labels[minibatch_idx:minibatch_idx+minibatch_size]
         )
 
+        # Stats
         avg_loss += loss
         avg_accuracy += accuracy
 
-        if epoch and epoch % stats_rate == 0:
+        if iteration and iteration % stats_rate == 0:
 
             avg_loss /= stats_rate
             avg_accuracy /= stats_rate
 
             print("\n\n=========================")
-            print("Epoch: ", epoch)
+            print("Iteration: ", iteration)
             print("Loss: ", avg_loss)
             print("Accuracy: {:.1%}".format(avg_accuracy))
 
@@ -110,32 +141,45 @@ def main():
             avg_loss = 0
             avg_accuracy = 0
 
-        epoch += 1
+        iteration += 1
+
+    # Final results
+    plt.ioff()
+    plt.clf()
+    plot_W(lc.W)
 
 
 class LinearClassifier:
 
     def __init__(self):
         """
-        Initialize the Weights with random small numers.
+        Initialize the Weights with random small numbers.
         10 classes
         3073 is 32x32x3 + 1(bias)
         """
-        self.W = np.random.randn(10, 3073) * 0.000001
+        self.W = np.random.randn(10, 3073) * 0.00001
 
     def eval(self, xi):
         """
+        Evaluate the scores for the input image xi
 
+        :param xi: a vector of 3072 elements representing the 32x32x3 image
+        :return:
         """
         return self.W.dot(np.append(xi, 1))
 
-    def train(self, X, y, learning_rate=1e-9, reg=3000):
+    def train(self, X, y, learning_rate=1e-7, reg=2.5e4):
         """
-        
+        Perform a training iteration of the linear classifier
+
+        :param X: Training data
+        :param y: Ground truth labels
+        :param learning_rate: Learning rate
+        :param reg: Regularization coefficient
+        :return:
         """
 
         n = len(y)
-        accuracy = .0
 
         # compute scores: W*x
         scores = self.W.dot(X)
@@ -148,11 +192,11 @@ class LinearClassifier:
         loss = np.sum(margins) / n
 
         # Compute hinge loss contribution to dW
-        mask = np.zeros(margins.shape)
-        mask[margins > 0] = 1
+        mask = np.zeros(scores.shape)
+        mask[margins > 0] = 1.0
+        dW = mask.dot(X.T) / n
 
-        dW = mask.dot(X.T)
-
+        # Compute the accuracy for statistical purposes
         accuracy = np.sum(np.argmax(scores, axis=0) == y) / n
 
         # Add regularization loss
@@ -168,5 +212,12 @@ class LinearClassifier:
 
 
 if __name__ == '__main__':
+
+    # Make sure to use Python 3
+    if sys.version_info[0] < 3:
+        raise Exception("Must be using Python 3")
+
+    # Always get the same pseudorandom numbers
     np.random.seed(0)
+
     main()